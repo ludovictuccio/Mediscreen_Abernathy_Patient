323b686de0f6343ad3c43ed9a0ce07b99a499ef7
Ludovic Tuccio <ludovic.tuccio@hotmail.fr>
2021-01-14 15:29:59 +0100

    API REST

--- src/main/java/com/mediscreen/patient/controllers/api/PatientControllerApiRest.java
+++ src/main/java/com/mediscreen/patient/controllers/api/PatientControllerApiRest.java
@@ -1,17 +1,32 @@
 package com.mediscreen.patient.controllers.api;
 
+import java.util.List;
+
+import javax.validation.Valid;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.mediscreen.patient.domain.Patient;
+import com.mediscreen.patient.repository.PatientRepository;
 import com.mediscreen.patient.services.PatientService;
+
+import io.swagger.annotations.ApiOperation;
 
 @RestController
 @Validated
-@RequestMapping(value = "/patient")
+@RequestMapping(value = "/api/patient")
 public class PatientControllerApiRest {
 
     private static final Logger LOGGER = LogManager
@@ -20,18 +35,53 @@ public class PatientControllerApiRest {
     @Autowired
     private PatientService patientService;
 
-//    @ApiOperation(value = "GET a list of all patients with the same lastname", notes = "Need param lastName - Return   or  bad request", response = Patient.class)
-//    @GetMapping("/searchPatient")
-//    public List<Patient> getAllUsers(@RequestParam String lastName) {
-//
-//        return patientService.getAllPatientsWithLastname(lastName);
-//    }
-//
-//    @ApiOperation(value = "GET the patient medical record", notes = "", response = Patient.class)
-//    @GetMapping("/medicalRecord")
-//    public Optional<Patient> getAllUsers(@PathVariable Long patId) {
-//
-//        return patientService.getPatientMedicalRecord(patId);
-//    }
+    @Autowired
+    private PatientRepository patientRepository;
+
+    @ApiOperation(value = "GET the list of all patients", notes = "Return response 200", response = Patient.class)
+    @GetMapping("/list")
+    public List<Patient> getAllMedicalRecords() {
+        return patientRepository.findAll();
+    }
+
+    @ApiOperation(value = "GET a list of all patients with the same lastname", notes = "Need param lastName - Return a list (empty if no lastName found) - Return 200", response = Patient.class)
+    @GetMapping("/searchPatient")
+    public List<Patient> getAllPatientsWithSameLastname(
+            @RequestParam String lastName) {
+        return patientService.getAllPatientsWithSameLastname(lastName);
+    }
+
+    @ApiOperation(value = "GET the patient medical record", notes = "Need param long 'patId' with patient's id - Return response 200", response = Patient.class)
+    @GetMapping
+    public Patient getMedicalRecord(@RequestParam Long patId) {
+        return patientService.getPatientMedicalRecord(patId);
+    }
+
+    @ApiOperation(value = "POST Add a new patient's medical record", notes = "Need Patient body - Return response 201 created or 400 bad request")
+    @PostMapping
+    public ResponseEntity<Patient> addPatient(
+            @Valid @RequestBody final Patient patient) {
+        Patient result = patientService.addPatient(patient);
+
+        if (result != null) {
+            return new ResponseEntity<Patient>(HttpStatus.CREATED);
+        }
+        LOGGER.error("POST request FAILED for: /api/patient");
+        return new ResponseEntity<Patient>(HttpStatus.BAD_REQUEST);
+    }
+
+    @ApiOperation(value = "PUT Update a patient's medical record", notes = "Need Patient body & param long 'patId' with patient's id - Return response 200 Ok or 400 bad request")
+    @PutMapping
+    public ResponseEntity<Boolean> updateMedicalRecord(
+            @Valid @RequestBody final Patient patient,
+            @RequestParam final Long patId) {
+        boolean result = patientService.updateMedicalRecord(patient, patId);
+
+        if (result == true) {
+            return new ResponseEntity<Boolean>(HttpStatus.OK);
+        }
+        LOGGER.error("POST request FAILED for: /api/patient");
+        return new ResponseEntity<Boolean>(HttpStatus.BAD_REQUEST);
+    }
 
 }
